5424468d4a8915b60c2829fc218a9b7b6c088593
Jason-Silvestre <jasonsilvestre43@yahoo.com>
2023-09-13 02:20:12 -0300

    Database seeding

--- backend/src/main/java/com/jasonsilvestre/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/jasonsilvestre/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.jasonsilvestre.dscatalog.entities.Category;
 import com.jasonsilvestre.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,8 +13,9 @@ public class CategoryService {
 
     @Autowired
     private CategoryRepository repository;
-    public List<Category> findAll() {
-        return  repository.findAll();
-    }
 
+    @Transactional(readOnly = true)
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
 }
