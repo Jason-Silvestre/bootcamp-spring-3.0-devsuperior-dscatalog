a540881b2e53bf9e807fb43563943a1ecc7e3cd7
Jason-Silvestre <jasonsilvestre43@yahoo.com>
2023-09-14 00:06:46 -0300

    Auditing data

--- backend/src/main/java/com/jasonsilvestre/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/jasonsilvestre/dscatalog/services/CategoryService.java
@@ -6,9 +6,12 @@ import com.jasonsilvestre.dscatalog.entities.Category;
 
 import com.jasonsilvestre.dscatalog.repositories.CategoryRepository;
 
+import com.jasonsilvestre.dscatalog.services.exceptions.DatabaseException;
 import com.jasonsilvestre.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import org.springframework.transaction.annotation.Transactional;
@@ -59,6 +62,18 @@ public class CategoryService {
         }
         catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Id not found " + id);
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Id not found "  +id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
         }
     }
 }
