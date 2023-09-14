0045e3290cbbb4c7cd9f67d36749bf0a635b472e
Jason-Silvestre <jasonsilvestre43@yahoo.com>
2023-09-14 00:33:46 -0300

    Pagination

--- backend/src/main/java/com/jasonsilvestre/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/jasonsilvestre/dscatalog/resources/CategoryResource.java
@@ -2,10 +2,13 @@ package com.jasonsilvestre.dscatalog.resources;
 
 import com.jasonsilvestre.dscatalog.dto.CategoryDTO;
 
-import com.jasonsilvestre.dscatalog.entities.Category;
 import com.jasonsilvestre.dscatalog.services.CategoryService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 import org.springframework.http.ResponseEntity;
 
@@ -13,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -21,9 +23,18 @@ public class CategoryResource {
 
     @Autowired
     private CategoryService service;
+
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findall() {
-        List<CategoryDTO> list = service.findAll();
+    public ResponseEntity<Page<CategoryDTO>> findall(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "DESC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+             ) {
+
+         PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+
+        Page<CategoryDTO> list = service.findAllPaged(pageRequest);
         return ResponseEntity.ok().body(list);
     }
 
@@ -40,6 +51,7 @@ public class CategoryResource {
                 .buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
     }
+
     @PutMapping(value = "/{id}")
     public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto) {
         dto = service.update(id, dto);
