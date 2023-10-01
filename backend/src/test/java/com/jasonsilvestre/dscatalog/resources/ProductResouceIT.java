6f179d8679f3aea3bbc62aea716a95e4f31297fb
Jason-Silvestre <jasonsilvestre43@yahoo.com>
2023-10-01 00:11:01 -0300

    tests chapter 2, ORM mapping, database seeding chapter 3

---
+++ backend/src/test/java/com/jasonsilvestre/dscatalog/resources/ProductResouceIT.java
@@ -0,0 +1,101 @@
+package com.jasonsilvestre.dscatalog.resources;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jasonsilvestre.dscatalog.dto.ProductDTO;
+import com.jasonsilvestre.dscatalog.tests.Factory;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.http.MediaType;
+
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ProductResouceIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    private Long existingId;
+    private Long nonExistingId;
+    private Long countTotalProducts;
+
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    public void findAllShouldReturnSortedPageWhenSortByName() throws Exception {
+
+        ResultActions result =
+                mockMvc.perform(get("/products?page=0&size=12&sort=name,asc")
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.totalElements").value(countTotalProducts));
+        result.andExpect(jsonPath("$.content").exists());
+        result.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
+        result.andExpect(jsonPath("$.content[1].name").value("PC Gamer"));
+        result.andExpect(jsonPath("$.content[2].name").value("PC Gamer Alfa"));
+    }
+
+    @Test
+    public void updateShouldReturnProductDTOWhenIdExists() throws Exception {
+
+        ProductDTO productDTO = Factory.createProductDTO();
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+        String description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+        String expectedName = productDTO.getName();
+        String expectedDescription = description;
+
+        ResultActions result =
+                mockMvc.perform(put("/products/{id}", existingId)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.id").value(existingId));
+        result.andExpect(jsonPath("$.name").value(expectedName));
+        result.andExpect(jsonPath("$.description").value(expectedDescription));
+    }
+
+    @Test
+    public void updateShouldReturnNotFoundWhenIdDoesNotExist() throws Exception {
+
+        ProductDTO productDTO = Factory.createProductDTO();
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+
+        ResultActions result =
+                mockMvc.perform(put("/products/{id}", nonExistingId)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isNotFound());
+    }
+}
---
+++ backend/src/test/java/com/jasonsilvestre/dscatalog/tests/Factory.java
@@ -0,0 +1,27 @@
+package com.jasonsilvestre.dscatalog.tests;
+
+import com.jasonsilvestre.dscatalog.dto.ProductDTO;
+import com.jasonsilvestre.dscatalog.entities.Category;
+import com.jasonsilvestre.dscatalog.entities.Product;
+
+import java.time.Instant;
+
+public class Factory {
+
+    public static Product createProduct() {
+        Product product = new Product(
+                1L, "Phone", "Good Phone", 800.0,
+                "https://img.com/img.png", Instant.parse("2020-10-20T03:00:00Z"));
+       product.getCategories().add(createCategory());
+       return product;
+    }
+
+    public static ProductDTO createProductDTO() {
+        Product product = createProduct();
+        return new ProductDTO(product, product.getCategories());
+    }
+
+    public static Category createCategory() {
+        return  new Category(1L, "Eletronics");
+    }
+}
