0178a36b42260bb88b3fb6661a667af4439478d7
Jason-Silvestre <jasonsilvestre43@yahoo.com>
2023-10-03 17:22:55 -0300

    User CRUD

---
+++ backend/src/main/java/com/jasonsilvestre/dscatalog/repositories/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.jasonsilvestre.dscatalog.repositories;
+
+import com.jasonsilvestre.dscatalog.entities.Role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
